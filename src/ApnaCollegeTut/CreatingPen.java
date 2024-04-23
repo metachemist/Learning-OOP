e8622d2b1d6c97960da273aef793d14ae1816b4a
metachemist <meta.chemist.22@gmail.com>
2024-04-23 23:42:03 +0500

    Adding CreatingPen

--- src/ApnaCollegeTut/CreatingPen.java
+++ src/ApnaCollegeTut/CreatingPen.java
@@ -4,14 +4,26 @@ class Pen{
     String color;
     String type;
 
-    public void write(){
-        System.out.println("Checking write method");
+    public void getColor() {
+        System.out.println(this.color);
     }
 
+    public void getType() {
+        System.out.println(this.type);
+    }
 }
 public class CreatingPen {
     public static void main(String []args){
         Pen pen1 = new Pen();
-        pen1.write();
+        pen1.color = "Blue";
+        pen1.type = "Fountain";
+        pen1.getType();
+        pen1.getColor();
+
+        Pen pen2 = new Pen();
+        pen2.color = "Black";
+        pen2.type = "Gel";
+        pen2.getColor();
+        pen2.getType();
     }
 }
