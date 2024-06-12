2505ca104b7171425b9c926ba9ef83bdc78719cb
metachemist <meta.chemist.22@gmail.com>
2024-06-12 11:16:44 +0500

    Changes in date.java and PracticeRandom.java

--- src/Chapter09/Date.java
+++
@@ -1,9 +0,0 @@
-package Chapter09;
-public class Date {
-
-    public static void main(String[] args) {
-        java.util.Date date = new java.util.Date();
-        System.out.println("The elapsed time since Jan 1, 1970 is " +date.getTime() + " millisecond" );
-        System.out.println(date.toString());
-    }
-}
--- src/Chapter09/PracticeRandom.java
+++ src/Chapter09/PracticeRandom.java
@@ -3,16 +3,15 @@ import java.util.Random;
 
 public class PracticeRandom {
     public static void main(String[] args) {
-        Random random = new Random();
+        Random random = new Random(3);
         System.out.println("From random1: ");
         for(int i=0; i<10; i++)
-            System.out.println(random.nextInt(1000) + " ");
+            System.out.print(random.nextInt(1000) + " ");
         
-        Random random2 = new Random();
-        System.out.println("From random2: ");
+        Random random2 = new Random(3);
+        System.out.println(" \nFrom random2: ");
         for(int i=0; i<10; i++)
-            System.out.println(random2.nextInt(1000) + " ");
-
+            System.out.print(random2.nextInt(1000) + " ");
 
     }
 }
