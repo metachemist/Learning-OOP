e6f522a9e5c33a7bf7483a130e3326cda1130bfd
metachemist <meta.chemist.22@gmail.com>
2024-06-16 12:15:07 +0500

    Chapter 09

--- src/Chapter09/A.java
+++ src/Chapter09/A.java
@@ -6,14 +6,14 @@ public class A {
     public static void main(String[] args) {
         A a = new A();
         int j = a.i;
-        a.m1;
+        a.m1();
     }
 
     public void m1(){
         i = i + k + m2(i,k);
     }
     public static int m2(int i, int j){
-        return (int) (Math.pow(i, j))
+        return (int) (Math.pow(i, j));
     }
 }
 
