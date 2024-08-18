fba22984ee3b29105ffd81ca4ceee670ddd0b7d8
metachemist <meta.chemist.22@gmail.com>
2024-08-18 18:09:24 +0500

    Adding partC.java

---
+++ src/PastPaperSolution/Question02/partC.java
@@ -0,0 +1,9 @@
+package PastPaperSolution.Question02;
+
+public class partC {
+    public static void main(String[] args){
+        double x = Math.PI;
+        int y = (int) Math.ceil(x); //ceil (rounds up the number to next digit)
+        System.out.println(y);
+    }
+}
