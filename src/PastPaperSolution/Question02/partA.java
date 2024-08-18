643182c3f6a9a864a0e1a57018dfd5be61febc39
metachemist <meta.chemist.22@gmail.com>
2024-08-18 16:54:53 +0500

    Adding partA.java

--- src/PastPaperSolution/Question02/partA.java
+++ src/PastPaperSolution/Question02/partA.java
@@ -1,13 +1,17 @@
 package PastPaperSolution.Question02;
 
-import java.sql.SQLOutput;
-
 class partA {
     public static void main(String[] args){
         int A = 10;
         int B = 20;
+        update(A,B);
         System.out.println(A+" "+B);
     }
-
-    public static void update(int X)
+    public static void update(int X, int Y){    //method
+        X = X + Y;
+        Y = Y + X;
+        System.out.println(X+" "+Y);
+    }
 }
+//no error
+
