41e0ee40f2741e6eec40a0155c902d9d9681e409
metachemist <meta.chemist.22@gmail.com>
2024-08-18 16:50:21 +0500

    Adding new package

---
+++ src/PastPaperSolution/Question02/partA.java
@@ -0,0 +1,13 @@
+package PastPaperSolution.Question02;
+
+import java.sql.SQLOutput;
+
+class partA {
+    public static void main(String[] args){
+        int A = 10;
+        int B = 20;
+        System.out.println(A+" "+B);
+    }
+
+    public static void update(int X)
+}
